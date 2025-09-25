dd83d36c77ed3e1a0bf7b7ec83a453a0e1c3c7fa
Amine <aminemefti@hotmail.com>
2025-09-25 12:47:42 -0400

    Add Footer and Header Template

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+     <p>&copy; {new Date().getFullYear()} SOEN341 Project </p> 
+    </footer>
+  );
+}
+
+export default Footer;
+/* Template. To change*/
---
+++ Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header>
+            <h1></h1>
+            <nav className="header-nav">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </nav>
+            <div className="header-bar"></div>
+        </header>
+    );
+}
+export default Header
+/*add hrefs later*/
+/*Template. To change*/
