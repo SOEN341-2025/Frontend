414374748c1c3f1279da1b1a76b2dcc421067141
Amine <aminemefti@hotmail.com>
2025-09-25 12:40:34 -0400

    Add LoginSignup.css and LoginSignup.jsx

---
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './login.css'
+import email from '../assets/email.png'
+import password from '../assets/password.png'
+import User_Login from '../assets/User_Login.png'
+
+export const Login = () => {
+  return (
+    <div className="container">
+        <div className="header">
+             <div className="text">Sign Up</div>
+                 <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src={User_Login} alt= "" className="icon"/>
+                <input type="text" placeholder="Name"/>
+            </div>
+            <div className="input">
+                <img src={email} alt= "" className="icon"/>
+                <input type="email" placeholder="Email"/>
+            </div>
+            <div className="input">
+                <img src={password} alt= ""  className="icon"/>
+                <input type="password" placeholder="Password"/>
+            </div>
+         </div>
+         <div className="forgot">Forgot Password? <span>Click Here!</span></div>
+
+         <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login</div>
+         </div>
+    </div>
+
+  )
+}
