beb4e97c35aeb4a48d1f355fbf3bbc9e314d07f5
Doki2403 <88411125+Sukhdarshan-Singh@users.noreply.github.com>
2025-10-03 21:41:53 -0400

    Corrected an error in an import statement.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
-import { LoginSignup } from './components/loginsignup/LoginSignup.jsx'
+import { LoginSignup } from './components/LoginSignup/LoginSignup.jsx'
 import React from 'react'
 
 function App() {
