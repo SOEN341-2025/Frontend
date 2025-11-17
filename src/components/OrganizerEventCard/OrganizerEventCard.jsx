34ffb41a0cf1f3b7674b0dd3ccd1b0d3156cb1aa
rima1881 <amd.gharibipour@outlook.com>
2025-11-17 15:50:49 -0500

    added react chart

--- src/components/OrganizerEventCard/OrganizerEventCard.jsx
+++ src/components/OrganizerEventCard/OrganizerEventCard.jsx
@@ -1,7 +1,22 @@
-
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 import styles from "./OrganizerEventCard.module.css"
 
 export default function OrganizerEventCard(props) {
+
+    const data = [
+        { time: "2025-11-17 10:00", value: 10 },
+        { time: "2025-11-17 11:00", value: 15 },
+        { time: "2025-11-17 12:00", value: 12 },
+        { time: "2025-11-17 13:00", value: 20 },
+    ];
 
     return(
         <div className={styles.container}>
@@ -10,7 +25,7 @@ export default function OrganizerEventCard(props) {
             <p>Location : {props.event.location}</p>
             <p>Date : {props.event.date}</p>
             <p>Price : ${props.event.price}</p>
-            <button>Get Analitics</button>
+            <a href={`/organization/${props.event.org_id}/event/${props.event.id}`}><button>Get Analitics</button></a>
         </div>
     )
 }
--- src/pages/event/Event.jsx
+++ src/pages/event/Event.jsx
@@ -1,14 +1,45 @@
 
 import { useParams } from "react-router-dom";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+
 
 function EventDetails() {
-  const { id } = useParams();
+  const { event_id } = useParams();
+
+    const data = [
+      { time: "2025-11-17 10:00", value: 10 },
+      { time: "2025-11-17 11:00", value: 15 },
+      { time: "2025-11-17 12:00", value: 12 },
+      { time: "2025-11-17 13:00", value: 20 },
+    ];
 
   return (
     <div>
       <h1>Event Details</h1>
-      <p>Event ID: {id}</p>
+      <p>Event ID: {event_id}</p>
+
+      <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
+
+
     </div>
+    
   );
 }
 
