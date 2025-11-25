c708bdd63a1ca675ea5d753fe6ff6d0d771cbbff
Doki2403 <88411125+Sukhdarshan-Singh@users.noreply.github.com>
2025-11-25 00:50:53 -0500

    Fixed Navbar

--- src/components/OrganizationCard/OrganizationCard.jsx
+++ src/components/OrganizationCard/OrganizationCard.jsx
@@ -4,7 +4,8 @@ import styles from "./OrganizationCard.module.css"
 export default function OrganizationCard(props) {
 
     return (
-    <div className={styles.container}>
+    <div className={styles.container}
+        >
         <img src={`http://localhost:3000/uploads/${props.src}`}/>
         <p>{props.name}</p>
         <a href={`/organization/${props.id}`}><button>View</button></a>
